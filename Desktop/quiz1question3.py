0bde4932fcb64ea803d4977833adaeeb528a8006
deepti <deepti11292@gmail.com>
2017-09-22 10:26:18 -0400

    modifying answer 3 comments

--- Desktop/quiz1question3.py
+++ Desktop/quiz1question3.py
@@ -13,8 +13,8 @@ print "Eigen Values: ", eigenvalues
 print "Eigen Vetors: ", eigenvectors
 
 ###############EIGENVALUES AND EIGENVECTORS##########################
-#Eigen Values:  [ 1.  2.]
+#Eigen Values:  [ 1.  2.], which are same the those I got on paper.
 #Eigen Vetors:  [[-0.70710678  0.4472136 ]
 # [ 0.70710678 -0.89442719]]
 
-#vALUES ARE MULTIPLES OF WHAT WE GET ON PAPER SOLVING MANUALLY.
+#EIGEN VECTORS ARE MULTIPLES OF WHAT I GOT ON PAPER SOLVING MANUALLY.
